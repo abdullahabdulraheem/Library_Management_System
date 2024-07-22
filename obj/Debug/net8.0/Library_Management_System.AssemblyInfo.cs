56096a9c61a86d946381d8dcf08204b0d77ab94b
Abdulhamiid <oyeboharemu@yahoo.com>
2024-07-22 09:54:20 +0100

    Updates

--- obj/Debug/net8.0/Library_Management_System.AssemblyInfo.cs
+++ obj/Debug/net8.0/Library_Management_System.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Library_Management_System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d063f3a9c4aeb1d5aa1e4ac4afb8a9d8e1478eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a86234fdbe399c26baac1e191b6db0b2932e3ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("Library_Management_System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Library_Management_System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
